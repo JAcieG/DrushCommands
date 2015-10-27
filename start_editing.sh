2ab5c2452e096db3fca27bc307f24d18d5d352f2
Joseph Gillam <jgillam@ashland.edu>
2015-10-27 09:19:30 -0400

    Add Files that Open every Sass File to edit and the one called start_editing it combines the opening of all sass files and opening of all terminal windows and opens firefox

---
+++ start_editing.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+gnome-terminal -x bash -c "cd /var/www/AshlandU/d7.ashlandu/docroot/sites/all/themes/ashland/sass; compass watch"
+gnome-terminal -x bash -c "cd /var/www/AshlandU/d7.ashlandu/docroot/sites/all/themes/ashlandgradadmis/sass; compass watch"
+gnome-terminal -x bash -c "cd /var/www/AshlandU/d7.ashlandu/docroot/sites/all/themes/ashlandadmissions/sass; compass watch"
+gnome-terminal -x bash -c "cd /var/www/AshlandU/d7.ashlandu/docroot/sites/all/themes/ashlandmba/sass; compass watch"
+gnome-terminal -x bash -c "cd /var/www/AshlandU/d7.ashlandu/docroot/sites/all/themes/ashlandiss/sass; compass watch"
+gnome-terminal -x bash -c "cd /var/www/AshlandU/d7.ashlandu/docroot/sites/all/themes/ashlanded/sass; compass watch"
+gnome-terminal -x bash -c "cd /var/www/AshlandU/d7.ashlandu/docroot/sites/all/themes/ashlandcobe/sass; compass watch"
+gnome-terminal -x bash -c "cd /var/www/AshlandU/d7.ashlandu/docroot/sites/all/themes/ashlandcon/sass; compass watch"
+gnome-terminal -x bash -c "cd /var/www/AshlandU/d7.ashlandu/docroot/sites/all/themes/ashlandarts/sass; compass watch"
+gnome-terminal -x bash -c "cd /var/www/AshlandU/d7.ashlandu/docroot/sites/all/themes/ashlandfounders/sass; compass watch"
+gnome-terminal -x bash -c "cd /var/www/AshlandU/d7.ashlandu/docroot/sites/all/themes/ashlanddevelop/sass; compass watch"
+gnome-terminal -x bash -c "cd /var/www/AshlandU/d7.ashlandu/docroot/sites/all/themes/ashlandstaf/sass; compass watch"
+gnome-terminal -x bash -c "cd /var/www/AshlandU/d7.ashlandu/docroot/sites/all/themes/ashlandadmin/sass; compass watch"
+gnome-terminal -x bash -c "cd /var/www/AshlandU/d7.ashlandu/docroot/sites/all/themes/ashlandmahg/sass; compass watch"
+xdg-open '/var/www/AshlandU/d7.ashlandu/docroot/sites/all/themes/ashlanded/sass/ashlanded-ashland-default-mobile.scss' 
+xdg-open '/var/www/AshlandU/d7.ashlandu/docroot/sites/all/themes/ashland/sass/ashland-ashland-default-mobile.scss'
+xdg-open '/var/www/AshlandU/d7.ashlandu/docroot/sites/all/themes/ashlandgradadmis/sass/ashlandgradadmis-ashland-default-mobile.scss'
+xdg-open '/var/www/AshlandU/d7.ashlandu/docroot/sites/all/themes/ashlandadmissions/sass/ashlandadmissions-ashland-default-mobile.scss'
+xdg-open '/var/www/AshlandU/d7.ashlandu/docroot/sites/all/themes/ashlandmba/sass/ashlandmba-ashland-default-mobile.scss'
+xdg-open '/var/www/AshlandU/d7.ashlandu/docroot/sites/all/themes/ashlandiss/sass/ashlandiss-ashland-default-mobile.scss'
+xdg-open '/var/www/AshlandU/d7.ashlandu/docroot/sites/all/themes/ashlandcobe/sass/ashlandcobe-ashland-default-mobile.scss'
+xdg-open '/var/www/AshlandU/d7.ashlandu/docroot/sites/all/themes/ashlandcon/sass/ashlandcon-ashland-default-mobile.scss'
+xdg-open '/var/www/AshlandU/d7.ashlandu/docroot/sites/all/themes/ashlandarts/sass/ashlandarts-ashland-default-mobile.scss'
+xdg-open '/var/www/AshlandU/d7.ashlandu/docroot/sites/all/themes/ashlandfounders/sass/ashlandfounders-ashland-default-mobile.scss'
+xdg-open '/var/www/AshlandU/d7.ashlandu/docroot/sites/all/themes/ashlanddevelop/sass/ashlanddevelop-ashland-default-mobile.scss'
+xdg-open '/var/www/AshlandU/d7.ashlandu/docroot/sites/all/themes/ashlandmahg/sass/ashlandmahg-ashland-default-mobile.scss'
+xdg-open '/var/www/AshlandU/d7.ashlandu/docroot/sites/all/themes/ashland/sass/all_sites_mobile/_ashland_all_sites_mobile.scss'
+xdg-open 'http://jgillam-d7.ashland.edu/'
